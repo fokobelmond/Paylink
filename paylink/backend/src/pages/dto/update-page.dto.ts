9aa25903e1e1447a13a61a7c5d117ea89b757d96
Neil Foko <fokobelmond@gmail.com>
2025-12-28 00:07:19 +0100

    Add files via upload

---
+++ paylink/backend/src/pages/dto/update-page.dto.ts
@@ -0,0 +1,12 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreatePageDto } from './create-page.dto';
+import { IsEnum, IsOptional } from 'class-validator';
+import { PageStatus } from '@prisma/client';
+
+export class UpdatePageDto extends PartialType(CreatePageDto) {
+  @IsOptional()
+  @IsEnum(PageStatus)
+  status?: PageStatus;
+}
+
+
